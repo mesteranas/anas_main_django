b834f467bed62f8679f49c128424083bdfa866fb
mesteranas <135442710+mesteranas@users.noreply.github.com>
2023-12-13 20:23:28 +0200

    13/12/2023

---
+++ anas_main/home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_(r):
+    return render(r,"home.html")
+def Contect(r):
+    return render(r,"contect.html")
+def about(r):
+    return render(r,"about.html")
